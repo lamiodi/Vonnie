53a45b5fb803800e3c2c59bb6008f721037c78bb
lamiodi <tygaodibenuah@gmail.com>
2026-03-07 10:04:37 +0100

    chore: remove unused clear_transactions script
    The script was used for manual database cleanup during development but is no longer needed in production. Removing it reduces repository clutter and prevents accidental execution.

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,38 @@
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'react-refresh'
+  ],
+  settings: {
+    react: {
+      version: '18.2'
+    }
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    'react/prop-types': 'off',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+  }
+}
